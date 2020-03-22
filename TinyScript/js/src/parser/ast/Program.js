8e739cebd600efa00cf6bb1477b3d9bde5a4ff4b
= <ramroll@gmail.com>
2020-03-22 13:11:01 +0800

    add program class

--- TinyScript/js/src/parser/ast/Program.js
+++ TinyScript/js/src/parser/ast/Program.js
@@ -1,6 +1,18 @@
-
+const ASTNodeTypes = require('./ASTNodeTypes')
 class Program {
-
+    constructor(){
+        super()
+    }
 }
 
 module.exports = Program
+
+const {Stmt} = require('./index')
+Program.parse = (it) => {
+    const program = new Program()
+    let stmt = null
+    while( (stmt = Stmt.parse(it)) != null) {
+        program.addChild(stmt)
+    }
+    return program
+}
