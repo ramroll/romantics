2aa540881dbaa3d4c1164ea99deaeaed305de777
maczyt <zhouyutao@youzan.com>
2020-05-11 14:21:02 +0800

    add linkedlist types

---
+++ TinyScript/unstable/typescript/src/common/PeekIterator.ts
@@ -0,0 +1,13 @@
+// @ts-ignore
+import LinkedList = require('linkedlist')
+
+/**
+ * 流处理
+ */
+class PeekIterator<T> {
+  constructor() {
+    const linkedlist = new LinkedList()
+  }
+}
+
+export default PeekIterator
---
+++ TinyScript/unstable/typescript/src/common/__tests__/PeekIterator.test.ts
@@ -0,0 +1,7 @@
+import PeekIterator from '../PeekIterator'
+
+describe('Name of the group', () => {
+  it('should ', () => {
+    new PeekIterator()
+  });
+});
